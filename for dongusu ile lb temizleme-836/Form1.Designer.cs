93fdccc9777733ca2ae784629b58315bb14f4d9e
ilayda yalçın <146716704+ilaydayacin@users.noreply.github.com>
2023-10-23 13:51:21 +0300

    Add files via upload

---
+++ for dongusu ile lb temizleme-836/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace for_dongusu_ile_lb_temizleme_836
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSinir = new System.Windows.Forms.TextBox();
+            this.lbSayilar = new System.Windows.Forms.ListBox();
+            this.btnHesap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(28, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sınır sayısı";
+            // 
+            // txtSinir
+            // 
+            this.txtSinir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSinir.Location = new System.Drawing.Point(31, 61);
+            this.txtSinir.Name = "txtSinir";
+            this.txtSinir.Size = new System.Drawing.Size(176, 26);
+            this.txtSinir.TabIndex = 1;
+            // 
+            // lbSayilar
+            // 
+            this.lbSayilar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbSayilar.FormattingEnabled = true;
+            this.lbSayilar.ItemHeight = 20;
+            this.lbSayilar.Location = new System.Drawing.Point(233, 44);
+            this.lbSayilar.Name = "lbSayilar";
+            this.lbSayilar.Size = new System.Drawing.Size(167, 324);
+            this.lbSayilar.TabIndex = 2;
+            // 
+            // btnHesap
+            // 
+            this.btnHesap.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesap.Location = new System.Drawing.Point(31, 120);
+            this.btnHesap.Name = "btnHesap";
+            this.btnHesap.Size = new System.Drawing.Size(119, 60);
+            this.btnHesap.TabIndex = 3;
+            this.btnHesap.Text = "Hesapla";
+            this.btnHesap.UseVisualStyleBackColor = true;
+            this.btnHesap.Click += new System.EventHandler(this.btnHesap_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(614, 450);
+            this.Controls.Add(this.btnHesap);
+            this.Controls.Add(this.lbSayilar);
+            this.Controls.Add(this.txtSinir);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSinir;
+        private System.Windows.Forms.ListBox lbSayilar;
+        private System.Windows.Forms.Button btnHesap;
+    }
+}
+
---
+++ for dongusu ile lb temizleme-836/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for_dongusu_ile_lb_temizleme_836
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesap_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(txtSinir.Text);
+            lbSayilar.Items.Clear();
+
+            for (int i=1; i<=sayi; i++)
+            {
+                lbSayilar.Items.Add(i.ToString());
+            }
+        }
+    }
+}
